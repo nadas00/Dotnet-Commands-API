0516fbe1068c4cf1fc2789ec643a466075ea6159
hasan çiftçi <se.hasancifti@gmail.com>
2020-06-17 13:00:32 +0300

    PATCH method implemented for patching a command by id

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+
 
 namespace Commander
 {
@@ -32,7 +34,9 @@ namespace Commander
             services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
             (Configuration.GetConnectionString("CommanderConnection")));
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(s => {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
             
             //for DTO 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
